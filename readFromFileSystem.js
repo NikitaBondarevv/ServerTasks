d0fa9945b8b78eb9d434359b6a088a1422d73f36
Nikita Bondarev <nikita.bondarev1807@gmail.com>
2022-09-19 13:43:30 +0300

    Fix bugs readFromFileSystem.js

--- readFromFileSystem.js
+++ readFromFileSystem.js
@@ -1,36 +1,23 @@
 const http = require('http');
 const fs = require('fs');
 const imageExtebsionsReg = /\.(png|jpeg|gif)$/;
-const extebsionJsReg = /\.(js)$/;
+const extebsionsJsReg = /\.(js)$/;
 
 const server = http.createServer((request, response) => {
   const fileName = request.url.replace('/', '');
   const errorHTML = '<strong>Error!</strong>';
   const errorText = `Error while reading ${fileName}`
 
-  if (imageExtebsionsReg.test(request.url)) {
-    fs.readFile(fileName, (err, data) => {
+  if (imageExtebsionsReg.test(request.url) || extebsionsJsReg.test(request.url)) {
+    fs.readFile(request.url.replace('/', ''), (err, data) => {
       if (err) {
         response.end(errorHTML);
         throw new Error(errorText);
       }
-
-      response.end(data);
-    })
-
-    return;
-  }
-
-  if (extebsionJsReg.test(request.url)) {
-    fs.readFile(fileName, (err, data) => {
-      if (err) {
-        response.end(errorHTML);
-        throw new Error(errorText);
-      };
   
       response.end(data);
-    });
-
+    })
+  
     return;
   }
 
